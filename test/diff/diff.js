caee1ff8685e256c5ac6472e8f154b65156ffa58
Aaron Lidman <aaronlidman@gmail.com>
2015-04-06 17:40:00 -0700

    Slightly working diff test

--- test/diff/diff.js
+++ test/diff/diff.js
@@ -1,11 +1,11 @@
 var turf = require('turf');
 
-module.exports = function(tileLayers, opts){
-  var streetsRoads = tileLayers.streets.roads;
+module.exports = function(tileLayers, opts) {
+  var streetsRoads = tileLayers.streets.road;
   streetsRoads.features = streetsRoads.features.map(function(road){
     return turf.buffer(road, 50, 'feet');
   });
-  var tigerRoads = tileLayers.tiger.roads;
+  var tigerRoads = tileLayers.tiger.tiger20062014;
   tigerRoads = turf.erase(streetsRoads, tigerRoads);
   return tigerRoads;
-}
+};
--- test/diff/index.js
+++ test/diff/index.js
@@ -1,5 +1,5 @@
 var test = require('tape');
-var tilereduce = new require('../../')();
+var TileReduce = require('../../');
 var diff = require('./diff.js');
 var turf = require('turf');
 
@@ -7,33 +7,34 @@ test('diff', function(t){
   var bbox = [
     -77.05810546875,
     38.913475954379756,
-    -77.04608917236328,
+    -78.04608917236328,
     38.92282516381189
-    ];
+  ];
 
   var opts = {
     zoom: 12,
     tileLayers: [
-        {
-          name: 'streets',
-          url: 'https://b.tiles.mapbox.com/v4/mapbox.mapbox-streets-v6/{z}/{x}/{y}.vector.pbf?access_token=pk.eyJ1IjoibW9yZ2FuaGVybG9ja2VyIiwiYSI6Ii1zLU4xOWMifQ.FubD68OEerk74AYCLduMZQ',
-          layers: ['roads']
-        },
-        {
-          name: 'tiger',
-          url: 'https://b.tiles.mapbox.com/v4/enf.rirltyb9-v6/{z}/{x}/{y}.vector.pbf?access_token=pk.eyJ1IjoibW9yZ2FuaGVybG9ja2VyIiwiYSI6Ii1zLU4xOWMifQ.FubD68OEerk74AYCLduMZQ',
-          layers: ['roads']
-        }
-      ],
-    map: diff
+      {
+        name: 'streets',
+        url: 'https://b.tiles.mapbox.com/v4/mapbox.mapbox-streets-v6/{z}/{x}/{y}.vector.pbf?access_token=pk.eyJ1IjoibW9yZ2FuaGVybG9ja2VyIiwiYSI6Ii1zLU4xOWMifQ.FubD68OEerk74AYCLduMZQ',
+        layers: ['road']
+      },
+      {
+        name: 'tiger',
+        url: 'https://b.tiles.mapbox.com/v4/enf.rirltyb9/{z}/{x}/{y}.vector.pbf?access_token=pk.eyJ1IjoibW9yZ2FuaGVybG9ja2VyIiwiYSI6Ii1zLU4xOWMifQ.FubD68OEerk74AYCLduMZQ',
+        layers: ['tiger20062014']
+      }
+    ],
+    map: __dirname+'/diff.js'
   };
 
+  var tilereduce = TileReduce(bbox, opts);
   var geojson = turf.featurecollection([]);
+
   tilereduce.on('start', function(tiles){
-    t.ok('tilereduce started');
     t.true(tiles.length > 0);
     tiles.forEach(function(tile) {
-      t.equal(tile[0].length, 3);
+      t.equal(tile.length, 3);
     });
   });
 
@@ -45,15 +46,13 @@ test('diff', function(t){
     t.true(geojson.features.length > 0, 'diff had features');
     var allLines = true;
     geojson.features.forEach(function(line){
-      if(!(line.geometry.type === 'LineString' || line.geometry.type === 'MultiLineString')){
+      if (!(line.geometry.type === 'LineString' || line.geometry.type === 'MultiLineString')) {
         allLines = false;
       }
     });
     t.true(allLines, 'all diff features were lines');
-
-    fs.writeFileSync(__dirname+'/out.geojson', JSON.stringify(geojson));
-    t.ok('tilereduce completed');
-
+    // // fs.writeFileSync(__dirname+'/out.geojson', JSON.stringify(geojson));
+    t.pass('tilereduce completed');
     t.end();
   });
 
@@ -61,5 +60,5 @@ test('diff', function(t){
     throw error;
   });
 
-  tilereduce(bbox, opts);
+  tilereduce.run();
 });
