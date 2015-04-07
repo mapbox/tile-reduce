d552a33c7b0c99d77dbb08f07c0e9b8540f632c2
Eric Fischer <enf@pobox.com>
2015-04-06 17:57:54 -0700

    Only consider an endpoint if its only exact match is itself.
    Still far too many false positives though.

--- test/disconnect/disconnect.js
+++ test/disconnect/disconnect.js
@@ -37,21 +37,31 @@ module.exports = function(tileLayers, opts){
     for (y in caps[x]) {
       if (caps[x][y] == 1) {
         var end = turf.point([Number(x), Number(y)]);
-        var best = Number.MAX_VALUE;
+        var exact = 0;
 
-        // check whether the end is close but not exactly on any other line
         streetsRoads.features.forEach(function(line2){
-          if (preserve[line2.properties.type]) {
-            var distance = turf.distance(end, turf.pointOnLine(line2, end));
-            if (distance < best && distance != 0) {
-              best = distance;
+          var i;
+          for (i = 0; i < line2.geometry.coordinates.length; i++) {
+            if (x == line2.geometry.coordinates[i][0] && y == line2.geometry.coordinates[i][1]) {
+              exact++;
             }
           }
         });
 
-        if (best < minDistance) {
-          end.properties.distance = best;
-          disconnects.features.push(end);
+        if (exact == 1) { // exactly matches only itself
+          var best = Number.MAX_VALUE;
+          streetsRoads.features.forEach(function(line2){
+            var distance = turf.distance(end, turf.pointOnLine(line2, end));
+            if (distance < best && distance != 0) {
+              best = distance;
+            }
+          });
+
+          if (best < minDistance) {
+            end.properties.distance = best;
+            disconnects.features.push(end);
+            console.log((best * 5280) + " http://www.openstreetmap.org/edit#map=24/" + y + "/" + x);
+          }
         }
       };
     }
