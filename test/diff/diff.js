5ff51192c928054357f3887875d3b81ce5eba5b4
Aaron Lidman <aaronlidman@gmail.com>
2015-04-07 17:29:28 -0700

    Diff progress

--- test/diff/diff.js
+++ test/diff/diff.js
@@ -2,10 +2,22 @@ var turf = require('turf');
 
 module.exports = function(tileLayers, opts) {
   var streetsRoads = tileLayers.streets.road;
-  streetsRoads.features = streetsRoads.features.map(function(road){
-    return turf.buffer(road, 50, 'feet');
-  });
+
   var tigerRoads = tileLayers.tiger.tiger20062014;
-  tigerRoads = turf.erase(streetsRoads, tigerRoads);
-  return tigerRoads;
+  tigerRoads.features = tigerRoads.features.map(function(road) {
+    return turf.buffer(road, 1, 'feet');
+  });
+  tigerRoads = turf.merge(tigerRoads);
+
+  streetsRoads.features = streetsRoads.features.map(function(road){
+    return turf.buffer(road, 50, 'feet').features[0];
+  });
+  streetsRoads = turf.merge(streetsRoads);
+
+  // we want
+  // var erase = turf.erase(tigerRoads, streetsRoads);
+  // but tigerRoads and streetsRoads are both multipolygons
+  // soooo, now what?
+
+  return streetsRoads;
 };
