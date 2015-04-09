afeda76e4e6ac734e5c14be551fabdd30e9a6538
Eric Fischer <enf@pobox.com>
2015-04-09 15:53:13 -0700

    Still a few false positives but close to the right thing now.

--- test/disconnect/disconnect.js
+++ test/disconnect/disconnect.js
@@ -6,90 +6,93 @@ module.exports = function(tileLayers, tile){
   var bbox = tilebelt.tileToBBOX(tile);
   var minDistance = 50/5280; // 50 ft in miles
   var disconnects = turf.featurecollection([]);
-  var preserve = { "motorway" : true, "primary" : true, "secondary" : true, "tertiary" : true, "trunk": true };
-  var caps = {};
-  var orig = {};
+  var caps = [];
+
+  // Types we are looking for disconnected ends for
+  var preserve_type = { "motorway" : true, "primary" : true, "secondary" : true, "tertiary" : true, "trunk": true };
+
+  // Classes that we don't want to suggest that they should connect to
+  var reject_class = { "major_rail" : true, "minor_rail" : true, "aerialway" : true };
 
 try {
   for (layer in tileLayers.streets) {
-    tileLayers.streets[layer].features.forEach(function(line){
-      // if (preserve[line.properties.type] && line.geometry.type === 'LineString') {
-      if (preserve[line.properties.type] && (line.geometry.type === 'LineString' || line.geometry.type === 'MultiLineString')) {
-        // get start and end points
+    var i, j, k;
+
+    for (i = 0; i < tileLayers.streets[layer].features.length; i++) {
+      var line = tileLayers.streets[layer].features[i];
+
+      if (preserve_type[line.properties.type] && (line.geometry.type === 'LineString' || line.geometry.type === 'MultiLineString')) {
         var ends = [
           line.geometry.coordinates[0],
           line.geometry.coordinates[line.geometry.coordinates.length-1]
         ];
 
-        // count how many times each endpoint appears
-        // so we can choose only those that are not already
-        // connected to something
+        // It's not dangling if it's a loop connecting back to itself
+        if (ends[0][0] == ends[1][0] && ends[0][1] == ends[1][1]) {
+          continue;
+        }
+
         ends.forEach(function(end){
           if (end[0] < bbox[0] || end[0] > bbox[2] || end[1] < bbox[1] || end[1] > bbox[3]) {
             return;
           }
 
-          if (!(end[0] in caps)) {
-            caps[end[0]] = {};
-            orig[end[0]] = {};
-          }
+          var dup = false;
 
-          if (!(end[1] in caps[end[0]])) {
-            caps[end[0]][end[1]] = 0;
-            orig[end[0]][end[1]] = 0;
-          }
-
-          caps[end[0]][end[1]]++;
-          orig[end[0]][end[1]] = end;
-        });
-      }
-    });
-  }
-
-  for (x in caps) {
-    for (y in caps[x]) {
-      if (caps[x][y] == 1) {
-        var end = turf.point([Number(x), Number(y)]);
-        var exact = 0;
-
-        for (layer in tileLayers.streets) {
-          tileLayers.streets[layer].features.forEach(function(line2){
-            var i;
-            for (i = 0; i < line2.geometry.coordinates.length; i++) {
-              if (x == line2.geometry.coordinates[i][0] && y == line2.geometry.coordinates[i][1]) {
-                exact++;
+          for (layer2 in tileLayers.streets) {
+            for (j = 0; j < tileLayers.streets[layer2].features.length; j++) {
+              if (layer === layer2 && i == j) {
+                continue;
               }
-            }
-          });
-        }
 
-        if (exact == 1) { // exactly matches only itself
-          var best = Number.MAX_VALUE;
-          for (layer in tileLayers.streets) {
-            tileLayers.streets[layer].features.forEach(function(line2){
-              if (line2.geometry.type === 'LineString' || line2.geometry.type === 'MultiLineString') {
-                var distance = turf.distance(end, turf.pointOnLine(line2, end));
-		if (distance < minDistance) {
-			console.log(distance + " " + JSON.stringify(line2));
-		}
-                if (distance < best && distance != 0) {
-                  best = distance;
-                  bestest = line2;
+              var line2 = tileLayers.streets[layer2].features[j];
+
+              for (k = 0; !dup && k < line2.geometry.coordinates.length; k++) {
+                if (end[0] === line2.geometry.coordinates[k][0] && end[1] === line2.geometry.coordinates[k][1]) {
+                  dup = true;
                 }
               }
-            });
+            }
           }
 
-          if (best < minDistance) {
-            end.properties.distance = best;
-            disconnects.features.push(end);
-            console.log((best * 5280) + " http://www.openstreetmap.org/edit#map=24/" + y + "/" + x + " " + JSON.stringify(end) + " " + JSON.stringify(orig[x][y]) + " " + JSON.stringify(bestest));
-		console.log("---------");
+          if (!dup) {
+            caps.push({layer: layer, i: i, point: turf.point(end), line: line});
+          }
+        });
+      }
+    }
+  }
+
+  caps.forEach(function(cap) {
+    var best = Number.MAX_VALUE;
+    var bestline = null;
+
+    for (layer in tileLayers.streets) {
+      var i;
+
+      for (i = 0; i < tileLayers.streets[layer].features.length; i++) {
+        if (layer == cap.layer && i == cap.i) {
+          continue;
+        }
+
+        var line = tileLayers.streets[layer].features[i];
+
+        if (!reject_class[line.properties.class] && (line.geometry.type === 'LineString' || line.geometry.type === 'MultiLineString')) {
+          var distance = turf.distance(cap.point, turf.pointOnLine(line, cap.point));
+
+          if (distance < best) {
+            best = distance;
+            bestline = line;
           }
         }
-      };
+      }
     }
-  };
+
+    if (best < minDistance && best != 0) {
+      disconnects.features.push(cap.point);
+      console.log((best * 5280) + " http://www.openstreetmap.org/edit#map=24/" + cap.point.geometry.coordinates[1] + "/" + cap.point.geometry.coordinates[0] + " " + JSON.stringify(cap.point) + " " + JSON.stringify(bestline));
+    }
+  });
 } catch (e) { console.log(e.stack) }
   
   // return points where distance is less than 50 feet
