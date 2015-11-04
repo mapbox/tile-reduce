94b8e2f6a0895eaa86fe2b4404d83027e92c7d12
Vladimir Agafonkin <agafonkin@gmail.com>
2015-11-04 17:29:08 +0200

    accept tile list as text file stream
    the tile file should be in the form of:
    x y z
    x y z

--- examples/count/index.js
+++ examples/count/index.js
@@ -6,7 +6,8 @@ var path = require('path');
 var numFeatures = 0;
 
 tileReduce({
-  bbox: [-131.693128, 30.664709, -100.681513, 55.35384],
+  tiles: path.join(__dirname, '../../../mbtiles/us-west.txt'),
+  // bbox: [-131.693128, 30.664709, -100.681513, 55.35384],
   zoom: 12,
   map: path.join(__dirname, '/count.js'),
   sources: [{name: 'osm', mbtiles: path.join(__dirname, '../../../mbtiles/us-west.mbtiles')}]
