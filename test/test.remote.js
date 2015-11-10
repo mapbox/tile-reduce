0ae0aa64614ff08d527b2cdc73a1bbf158fccac3
morganherlocker <morgan@mapbox.com>
2015-11-10 15:11:21 -0800

    tests for 401 status and request errors

--- test/test.remote.js
+++ test/test.remote.js
@@ -30,3 +30,29 @@ test('remote - init', function(t) {
     t.end();
   });
 });
+
+test('remote - raw invalid url - 401', function(t) {
+  var badUrl = 'https://b.tiles.mapbox.com/v4/morganherlocker.3vsvfjjw/{z}/{x}.vector.pbf';
+  var source = {name: 'osm', url: badUrl};
+  var getTile = remote(source, function() {});
+
+  getTile([5276, 12757, 15], function(err, layers) {
+    t.ok(err, 'returns an error with a bad remote url');
+    t.equal(err.message, 'Server responded with status code 401', '401 error');
+    t.notOk(layers, 'does not return layers with a bad remote url');
+    t.end();
+  });
+});
+
+test('remote - raw invalid url - no server', function(t) {
+  var badUrl = 'https://tiles.mapox.com/v4/morganherlocker.3vsvfjjw/{z}/{x}.vector.pbf';
+  var source = {name: 'osm', url: badUrl};
+  var getTile = remote(source, function() {});
+
+  getTile([5276, 12757, 15], function(err, layers) {
+    t.ok(err, 'returns an error with a bad remote url');
+    t.equal(err.message, 'getaddrinfo ENOTFOUND', 'HTTP address not found');
+    t.notOk(layers, 'does not return layers with a bad remote url');
+    t.end();
+  });
+});
