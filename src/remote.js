96cfedc881d57690bc5e8d6387cc1d95b9801906
morganherlocker <morgan@mapbox.com>
2015-11-10 15:11:21 -0800

    tests for 401 status and request errors

--- src/remote.js
+++ src/remote.js
@@ -22,8 +22,9 @@ function remoteVT(config, ready) {
       .replace('{z}', tile[2]);
 
     tileRequest(url, function(err, res, body) {
-      if (err) done(err);
-      else done(null, res.statusCode === 200 ? new VectorTile(new Pbf(body)).layers : null);
+      if (err) return done(err);
+      else if (res.statusCode !== 200) return done(new Error('Server responded with status code ' + res.statusCode));
+      else return done(null, new VectorTile(new Pbf(body)).layers);
     });
   };
 }
