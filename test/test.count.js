b2fa4b081e1c2b4326e291d39e962957d9d8e5a1
morganherlocker <morgan@mapbox.com>
2015-11-11 23:06:36 -0800

    1st end-to-end test

---
+++ test/test.count.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var test = require('tap').test;
+var tileReduce = require('../src');
+var path = require('path');
+
+test('count implementation', function(t) {
+  var numFeatures = 0;
+  var reduceFired = false;
+
+  tileReduce({
+    bbox: [-122.05862045288086, 36.93768132842635, -121.97296142578124, 37.00378647456494],
+    zoom: 15,
+    map: path.join(__dirname, 'fixtures/count.js'),
+    sources: [
+      {name: 'osm', mbtiles: path.join(__dirname, '/fixtures/osm.mbtiles')},
+      {name: 'tiger', mbtiles: path.join(__dirname, '/fixtures/tiger.mbtiles')}
+    ]
+  })
+  .on('reduce', function(num) {
+    numFeatures += num;
+    reduceFired = true;
+  })
+  .on('end', function() {
+    t.equal(numFeatures, 16182, 'found all features');
+    t.equal(reduceFired, true, 'reduce fired');
+    t.end();
+  });
+});
