aad3aa97f6e89a4ba771702d1f1f8d86c52ff99f
morganherlocker <morgan@mapbox.com>
2015-11-12 15:59:03 -0800

    add geojson parsing for remote sources

--- src/remote.js
+++ src/remote.js
@@ -12,11 +12,11 @@ var tileRequest = request.defaults({
   agentOptions: {maxSockets: 5}
 });
 
-function remoteVT(config, ready) {
+function remoteVT(source, ready) {
   ready();
 
   return function(tile, done) {
-    var url = config.url
+    var url = source.url
       .replace('{x}', tile[0])
       .replace('{y}', tile[1])
       .replace('{z}', tile[2]);
@@ -24,7 +24,28 @@ function remoteVT(config, ready) {
     tileRequest(url, function(err, res, body) {
       if (err) return done(err);
       else if (res.statusCode !== 200) return done(new Error('Server responded with status code ' + res.statusCode));
-      else return done(null, new VectorTile(new Pbf(body)).layers);
+      else if (source.raw) done(null, new VectorTile(new Pbf(body)).layers);
+      else done(null, toGeoJSON(new VectorTile(new Pbf(body)), tile, source));
     });
   };
+}
+
+function toGeoJSON(vt, tile, source) {
+  var layers = Object.keys(vt.layers);
+  var collections = {};
+
+  for (var i = 0; i < layers.length; i++) {
+    if (!source.layers || source.layers.indexOf(layers[i]) !== -1) {
+      collections[layers[i]] = {
+        type: 'FeatureCollection',
+        features: []
+      };
+      for (var k = 0; k < vt.layers[layers[i]].length; k++) {
+        collections[layers[i]].features.push(
+          vt.layers[layers[i]].feature(k).toGeoJSON(tile[0], tile[1], tile[2])
+        );
+      }
+    }
+  }
+  return collections;
 }
