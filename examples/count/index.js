117c44303d0b2a257b55cf4d23ba552524028068
Emily Grasmeder <emma@gras.org>
2016-05-06 14:37:03 -0700

    we only do this at z12

--- examples/count/index.js
+++ examples/count/index.js
@@ -4,10 +4,10 @@ var tileReduce = require('../../src');
 var path = require('path');
 
 var numFeatures = 0;
-
+m
 tileReduce({
   bbox: [-122.05862045288086, 36.93768132842635, -121.97296142578124, 37.00378647456494],
-  zoom: 15,
+  zoom: 12,
   map: path.join(__dirname, '/count.js'),
   sources: [{name: 'osm', mbtiles: path.join(__dirname, '../../test/fixtures/osm.mbtiles'), raw: true}]
 })
